f6d4af912ae847a5fa011d272f5ab51e54fb6d09
Paul Trafford <31771946+paultraf@users.noreply.github.com>
2023-03-21 17:50:37 +0000

    Updated version history for 0.23.1

--- lib/constants.sh
+++ lib/constants.sh
@@ -22,8 +22,8 @@
 ################################################
 # MakeStaticSite info
 ################################################
-version="0.23"
-version_date='18 March 2023'
+version="0.23.1"
+version_date='21 March 2023'
 version_header="MakeStaticSite version $version, released on $version_date."
 mss_license="GNU Affero General Public License version 3"
 mss_download="https://makestaticsite.sh/download/makestaticsite_latest.tar.gz"
