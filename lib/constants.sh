de0afcb25f787ce04c1048c833a29f8b7281d1c9
Paul Trafford <31771946+paultraf@users.noreply.github.com>
2024-05-29 12:08:16 +0100

    Upped version to 0.29.8

--- lib/constants.sh
+++ lib/constants.sh
@@ -22,7 +22,7 @@
 ################################################
 # MakeStaticSite info
 ################################################
-version=0.29.8-beta
+version=0.29.8
 version_date='29 May 2024'
 version_header="MakeStaticSite version $version, released on $version_date."
 mss_license="GNU Affero General Public License version 3"
