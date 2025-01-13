643cfe7c320b355b3ea2c7d135f252607053be39
Paul Trafford <31771946+paultraf@users.noreply.github.com>
2025-01-13 19:54:28 +0000

    Upped version to 0.31

--- lib/constants.sh
+++ lib/constants.sh
@@ -22,8 +22,8 @@
 ################################################
 # MakeStaticSite info
 ################################################
-version=0.30.13.2
-version_date='8 January 2025'
+version=0.31
+version_date='13 January 2025'
 version_header="MakeStaticSite version $version, released on $version_date."
 mss_license="GNU Affero General Public License version 3"
 mss_site="https://makestaticsite.sh"
